bde9238ce7254ba9ec3d354b64f11944e2fccb21
Danish <danishsidd524@gmail.com>
2025-07-07 23:48:44 +0500

    setup project 1

---
+++ project-1/terraform/variables.tf
@@ -0,0 +1,35 @@
+# General variables
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "app_name" {
+  description = "Name of the application"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment for the application (e.g., dev, prod)"
+  type        = string
+  default     = "dev"
+
+  validation {
+    condition     = contains(["prod", "dev", "test"], var.environment)
+    error_message = "Environment must be one of 'prod', 'dev', or 'test'."
+  }
+}
+
+# Compute
+variable "ami" {
+  description = "AMI ID for the instance"
+  type        = string
+  default     = "ami-02521d90e7410d9f0"
+}
+
+variable "instance_type" {
+  description = "Type of instance to use"
+  type        = string
+  default     = "t2.micro"
+}
