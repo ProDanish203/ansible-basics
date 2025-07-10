5533ddbe1031acd71700adf3bb8319e7956a4edf
Danish <danishsidd524@gmail.com>
2025-07-10 23:04:40 +0500

    add: VPC and public/private subnets

---
+++ project-2/infra/subnets.tf
@@ -0,0 +1,22 @@
+resource "aws_subnet" "public_subnets" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.private_subnet_cidrs, count.index)
+  map_public_ip_on_launch = true
+  availability_zone       = element(var.azs, count.index)
+
+  tags = {
+    Name = "${var.app_name}-${var.environment}-public-subnet=${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.private_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+
+  tags = {
+    Name = "${var.app_name}-${var.environment}-private-subnet-${count.index + 1}"
+  }
+}
